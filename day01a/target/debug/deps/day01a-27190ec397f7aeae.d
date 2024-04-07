e077aeae7897014659ca05731b60075fb109c53b
Jamie <75147459+jamiethomas1@users.noreply.github.com>
2024-04-07 17:39:38 +0100

    Delete day01a/target directory

--- day01a/target/debug/deps/day01a-27190ec397f7aeae.d
+++
@@ -1,5 +0,0 @@
-/home/jamie/Documents/software-dev/rust/projects/advent-of-code-2015/day01a/target/debug/deps/libday01a-27190ec397f7aeae.rmeta: src/main.rs
-
-/home/jamie/Documents/software-dev/rust/projects/advent-of-code-2015/day01a/target/debug/deps/day01a-27190ec397f7aeae.d: src/main.rs
-
-src/main.rs:
--- day01a/target/debug/deps/day01a-7d8c27dc553ddb5d.d
+++
@@ -1,5 +0,0 @@
-/home/jamie/Documents/software-dev/rust/projects/advent-of-code-2015/day01a/target/debug/deps/libday01a-7d8c27dc553ddb5d.rmeta: src/main.rs
-
-/home/jamie/Documents/software-dev/rust/projects/advent-of-code-2015/day01a/target/debug/deps/day01a-7d8c27dc553ddb5d.d: src/main.rs
-
-src/main.rs:
