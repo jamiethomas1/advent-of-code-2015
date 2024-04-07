2678ae0f22cec0e048f3443c2606fdd37b5d1d4c
cogsincogs <jamiewthomas13@gmail.com>
2024-04-07 18:02:01 +0100

    Solve day01a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_bytes!("../input.txt");
+    // println!("{}", String::from_utf8_lossy(input));
+    let mut floor = 0;
+    input.bytes().for_each(|b| {
+        match b {
+            Ok(b'(') => floor += 1,
+            Ok(b')') => floor -= 1,
+            _ => return,
+        }
+    });
+
+    println!("{floor}");
 }
